62815252c888ca49201f0546ad77aed3610c6436
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-05-12 07:50:10 +0300

    Update bluetooth.ino

--- bluetooth.ino
+++ bluetooth.ino
@@ -2,21 +2,27 @@
 #define INCLUDE_GAMEPAD_MODULE
 #include <Dabble.h>
 
+int signalR = 12;
+int signalL = 11;
 int headlight = 13;
 int horn = 4;
-int right1 = 9;
-int right2 = 8;
-int left1 = 6;
-int left2 = 7;
+
+int motorR1 = 9;
+int motorR2 = 8;
+int motorL1 = 6;
+int motorL2 = 7;
 
 void setup() {
 
+  pinMode(signalR, OUTPUT);
+  pinMode(signalL, OUTPUT);
   pinMode(headlight, OUTPUT);
   pinMode(horn, OUTPUT);
-  pinMode(right1, OUTPUT);
-  pinMode(right2, OUTPUT);
-  pinMode(left1, OUTPUT);
-  pinMode(left2, OUTPUT);
+  
+  pinMode(motorR1, OUTPUT);
+  pinMode(motorR2, OUTPUT);
+  pinMode(motorL1, OUTPUT);
+  pinMode(motorL2, OUTPUT);
 
   Serial.begin(9600);
   Dabble.begin(9600);
@@ -26,52 +32,61 @@ void loop() {
   Dabble.processInput();
 
   if (GamePad.isUpPressed()) {
-    digitalWrite(right1, LOW);
-    digitalWrite(right2, HIGH);
-    digitalWrite(left1, LOW);
-    digitalWrite(left2, HIGH);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, HIGH);
   }
 
   else if (GamePad.isDownPressed()) {
-    digitalWrite(right1, HIGH);
-    digitalWrite(right2, LOW);
-    digitalWrite(left1, HIGH);
-    digitalWrite(left2, LOW);
+    digitalWrite(motorR1, HIGH);
+    digitalWrite(motorR2, LOW);
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
   }
 
   else if (GamePad.isLeftPressed()) {
-    digitalWrite(right1, LOW);
-    digitalWrite(right2, HIGH);
-    digitalWrite(left1, HIGH);
-    digitalWrite(left2, LOW);
+    digitalWrite(signalL, (millis() / 350) % 2);
+    
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
   }
 
   else if (GamePad.isRightPressed()) {
-    digitalWrite(right1, HIGH);
-    digitalWrite(right2, LOW);
-    digitalWrite(left1, LOW);
-    digitalWrite(left2, HIGH);
+    digitalWrite(signalR, (millis() / 350) % 2);
+    
+    digitalWrite(motorR1, HIGH);
+    digitalWrite(motorR2, LOW);
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, HIGH);
   }
-
-  else if (GamePad.isCrossPressed()) {
+  
+  else if (GamePad.isSquarePressed()) {
     tone(horn, 750, 10);
   }
 
   else if (GamePad.isCirclePressed()) {
+    
     if (digitalRead(headlight) == LOW) {
       digitalWrite(headlight, HIGH);
       delay(250);
     }
-    else if (digitalRead(headlight) == HIGH){
+    
+    else if (digitalRead(headlight) == HIGH) {
       digitalWrite(headlight, LOW);
       delay(250);
     }
   }
 
   else {
-    digitalWrite(right1, LOW);
-    digitalWrite(right2, LOW);
-    digitalWrite(left1, LOW);
-    digitalWrite(left2, LOW);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, LOW);
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, LOW);
+    
+    digitalWrite(signalR, LOW);
+    digitalWrite(signalL, LOW);
   }
 }
